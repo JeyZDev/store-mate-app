a67de091f28eaea7a56798c36ff3cff61ee6acf8
JeyZDev <664259015@webmail.npru.ac.th>
2026-02-09 09:57:28 +0700

    test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,18 @@
-import { useContext } from "react";
 import { BiSolidBell } from "react-icons/bi";
 import { FaRegUser, FaCartShopping } from "react-icons/fa6";
 import { FiSearch } from "react-icons/fi";
-import { UserContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
+import { useSelector } from "react-redux";
+import type { RootState } from "../redux/store";
+import UserAvatar from "./UserAvatar";
 
 const Navbar: React.FC = () => {
-  const context = useContext(UserContext);
-
-  if (!context) {
-    throw new Error("UserContext must be used within UserContextProvider");
-  }
-
-  const { isAuthenticated, logout } = context;
   const navigate = useNavigate();
 
-const handleLogout = () => {
-  Swal.fire({
-    title: "ออกจากระบบ?",
-    text: "คุณต้องการออกจากระบบใช่หรือไม่",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonText: "ออกจากระบบ",
-    cancelButtonText: "ยกเลิก",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      logout();
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
 
-      Swal.fire({
-        title: "ออกจากระบบสำเร็จ",
-        icon: "success",
-        timer: 1500,
-        showConfirmButton: false,
-      }).then(() => {
-        navigate("/");
-      });
-    }
-  });
-};
   return (
     <div className="navbar bg-base-100 shadow-sm h-[101px] w-full bg-gray-100">
       {/* LEFT */}
@@ -52,7 +26,7 @@ const handleLogout = () => {
           <li><a>Product</a></li>
           <li><a>Promotion</a></li>
           <li><a>About us</a></li>
-             <li><a>Contact</a></li>
+          <li><a>Contact</a></li>
         </ul>
       </div>
 
@@ -63,31 +37,11 @@ const handleLogout = () => {
         <BiSolidBell size={24} />
 
         {isAuthenticated ? (
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="cursor-pointer">
-              <div className="w-10 h-10 rounded-full flex items-center justify-center">
-                <FaRegUser  size={24} />
-              </div>
-            </label>
-
-            <ul className="menu dropdown-content bg-white w-40 p-2 shadow">
-              <li className="text-sm text-black">👋 ยินดีต้อนรับ</li>
-              <li>
-                <button
-                  className="text-red-500 font-bold"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
+          <UserAvatar />
         ) : (
-          <FaRegUser
-            size={24}
-            className="cursor-pointer"
-            onClick={() => navigate("/login")}
-          />
+          <button onClick={() => navigate("/login")}>
+            <FaRegUser size={24} className="cursor-pointer" />
+          </button>
         )}
       </div>
     </div>
